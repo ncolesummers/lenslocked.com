a5c40a508c2bac2ab5c369853313c123e957bd24
ncolesummers <nsummers72@gmail.com>
2020-09-09 14:44:10 -0700

    Created our users controller and updated main.go to use it instead of the signup function

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"net/http"
-
+	"./controllers"
 	"./views"
 	"github.com/gorilla/mux"
 )
@@ -10,7 +10,6 @@ import (
 var (
 	homeView    *views.View
 	contactView *views.View
-	signupView *views.View
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -23,20 +22,17 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	must(contactView.Render(w, nil))
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	must(signupView.Render(w, nil))
-}
+
 
 func main() {
 	homeView = views.NewView("bootstrap", "views/home.gohtml")
 	contactView = views.NewView("bootstrap", "views/contact.gohtml")
-	signupView = views.NewView("bootstrap", "views/signup.gohtml")
+	usersC := controllers.NewUsers()
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/signup", signup)
+	r.HandleFunc("/signup", usersC.New)
 	must(http.ListenAndServe(":3000", r))
 }
 
