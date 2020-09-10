149abab92e66dd572caa34a4b840421b479d7aa6
ncolesummers <nsummers72@gmail.com>
2020-09-09 18:10:09 -0700

    Wrote a first draft of code to parse the signup form

--- controllers/users.go
+++ controllers/users.go
@@ -31,5 +31,10 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, r.PostFormValue("email"))
+	fmt.Fprintln(w, r.PostFormValue("password"))
 	fmt.Fprintln(w, "This is a temporary response.")
 }
